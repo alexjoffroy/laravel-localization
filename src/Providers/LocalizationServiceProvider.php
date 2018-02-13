6ecb3735b7adc55411a79bbf1af59c833332139d
alexjoffroy <alexandre.joffroy@gmail.com>
2018-02-13 08:27:24 +0100

    It registers locale routes

--- src/Providers/LocalizationServiceProvider.php
+++ src/Providers/LocalizationServiceProvider.php
@@ -2,22 +2,38 @@
 
 namespace AlexJoffroy\LaravelLocalization\Providers;
 
+use Closure;
 use Illuminate\Support\ServiceProvider;
 use AlexJoffroy\LaravelLocalization\LocalizationManager;
 
 class LocalizationServiceProvider extends ServiceProvider
 {
-    public function boot()
-    {
-    }
-
     public function register()
     {
         $this->app->singleton('localization', function () {
             return new LocalizationManager($this->app);
         });
 
-        $this->app['router']->macro('locales', function (Closure $closure) {
+        $this->registerRouteLocalesMacro();
+    }
+
+    public function registerRouteLocalesMacro()
+    {
+        $router = $this->app['router'];
+        $l10n = $this->app['localization'];
+        $router->macro('locales', function (Closure $closure) use ($router, $l10n) {
+            $locales = $l10n->getSupportedLocalesKeys();
+            $currentLocale = $l10n->getLocale();
+            foreach ($locales as $locale) {
+                $l10n->setLocale($locale);
+                $router->group([
+                    'as' => "$locale.",
+                    'prefix' => $locale,
+                ], function () use ($closure) {
+                    $closure();
+                });
+            }
+            $l10n->setLocale($currentLocale);
         });
     }
 }
--- tests/RouteLocalesMacroTest.php
+++ tests/RouteLocalesMacroTest.php
@@ -2,14 +2,44 @@
 
 namespace AlexJoffroy\LaravelLocalization\Tests;
 
-use Illuminate\Support\Facades\Route;
 use AlexJoffroy\LaravelLocalization\LocalizationManager;
 
 class RouteLocalesMacroTest extends TestCase
 {
+    protected $locales = [
+        'en' => ['native' => 'English'],
+        'fr' => ['native' => 'Français'],
+    ];
+    
     /** @test */
-    public function it_register_the_locales_macro_on_router()
+    public function it_registers_the_locales_macro_on_router()
     {
         $this->assertTrue($this->app['router']->hasMacro('locales'));
+    }
+
+    /** @test */
+    public function it_registers_locales_routes()
+    {
+        config([
+            'localization.supported_locales' => $this->locales,
+            'localization.default_locale' => 'en'
+        ]);
+        $this->app['translator']->addLines(['routes.home' => 'home'], 'en');
+        $this->app['translator']->addLines(['routes.home' => 'accueil'], 'fr');
+            
+        $router = $this->app['router'];
+        $router->locales(function () use ($router) {
+            $router->get(trans('routes.home'), [
+                'as' => 'home',
+                'uses' => function () {
+                    return 'home';
+                }
+            ]);
+        });
+        
+        $this->assertTrue($router->has('en.home'));
+        $this->assertTrue($router->has('fr.home'));
+        $this->assertEquals('/en/home', route('en.home', [], false));
+        $this->assertEquals('/fr/accueil', route('fr.home', [], false));
     }
 }
